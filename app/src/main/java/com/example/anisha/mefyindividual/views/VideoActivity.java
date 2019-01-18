b99a7ffe113610d2431ffbc1425031d72289207d
Usha <usha.kiran@colorbracket.com>
2019-01-18 12:51:38 +0530

    VideoActivity modified for Wake Lock,Calling UI Changed Text Size.

--- app/src/main/java/com/example/anisha/mefyindividual/views/VideoActivity.java
+++ app/src/main/java/com/example/anisha/mefyindividual/views/VideoActivity.java
@@ -120,21 +120,20 @@ public class VideoActivity extends AppCompatActivity {
 
         Window window = getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED| WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON| WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        // to wake up screen
+        /*// to wake up screen
         PowerManager pm = (PowerManager) getApplicationContext().getSystemService(Context.POWER_SERVICE);
         @SuppressLint("InvalidWakeLockTag") PowerManager.WakeLock wakeLock = pm.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "TAG");
         wakeLock.acquire();
         if ((wakeLock != null) &&           // we have a WakeLock
                 (wakeLock.isHeld() == false)) {  // but we don't hold it
             wakeLock.acquire();
-        }
+        }*/
 
 
         // to release screen lock
         KeyguardManager keyguardManager = (KeyguardManager) getApplicationContext().getSystemService(Context.KEYGUARD_SERVICE);
         KeyguardManager.KeyguardLock keyguardLock = keyguardManager.newKeyguardLock("TAG");
         keyguardLock.disableKeyguard();
-
 /*// Share your camera
         CameraCapturer cameraCapturer = new CameraCapturer(this, CameraCapturer.CameraSource.FRONT_CAMERA);
         localVideoTrack = LocalVideoTrack.create(this, true, cameraCapturer);
